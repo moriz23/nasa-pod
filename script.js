6ba87ccc3d0d7c88a52446e0202a170d7cef6cd0
Mohammed Rizvi <morizvi07@gmail.com>
2021-01-11 14:31:46 -0500

    Added files to nasa pod repo

---
+++ script.js
@@ -0,0 +1,96 @@
+const resultsNav = document.getElementById('resultsNav')
+const favoritesNav = document.getElementById('favoritesNav')
+const imagesContainer = document.querySelector('.images-container')
+const saveConfirmed = document.querySelector('.save-confirmed')
+const loader = document.querySelector('.loader')
+
+// NASA API
+const count = 10
+const apiKey = 'DEMO_KEY'
+const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`
+
+let resultsArray = []
+let favorites = {}
+
+function updateDOM () {
+  resultsArray.forEach(result => {
+    // Card Container
+    const card = document.createElement('div')
+    card.classList.add('card')
+    // Link
+    const link = document.createElement('a')
+    link.href = result.hdurl
+    link.title = 'View Full Image'
+    link.target = '_blank'
+    // Image
+    const image = document.createElement('img')
+    image.src = result.url
+    image.alt = 'NASA Picture of the Day'
+    image.loading = 'lazy'
+    image.classList.add('card-img-top')
+    // Card Body
+    const cardBody = document.createElement('div')
+    cardBody.classList.add('card-body')
+    // Card Title
+    const cardTitle = document.createElement('h5')
+    cardTitle.classList.add('card-title')
+    cardTitle.textContent = result.title
+    // Save Text
+    const saveText = document.createElement('p')
+    saveText.classList.add('clickable')
+    saveText.textContent = 'Add To Favorites'
+    saveText.setAtrribute('onclick', `saveFavorite('${result.url}')`)
+    // Card Text
+    const cardText = document.createElement('p')
+    cardText.textContent = result.explanation
+    // Footer Container
+    const footer = document.createElement('small')
+    footer.classList.add('text-muted')
+    // Date
+    const date = document.createElement('strong')
+    date.textContent = result.date
+    // Copyright
+    const copyRightResult =
+      result.copyright === undefined ? '' : result.copyright
+    const copyright = document.createElement('span')
+    copyright.textContent = ` ${copyRightResult}`
+    // Append
+    footer.append(date, copyright)
+    cardBody.append(cardTitle, saveText, cardText, footer)
+    link.appendChild(image)
+    card.append(link, cardBody)
+    imagesContainer.appendChild(card)
+  })
+}
+
+// GET 10 IMAGES FROM NASA API
+async function getNasaPictures () {
+  try {
+    const response = await fetch(apiUrl)
+    resultsArray = await response.json()
+    console.log(resultsArray)
+    updateDOM()
+  } catch (error) {
+    //Catch Error Here
+  }
+}
+
+//Add result to Favorites
+function saveFavorite (itemUrl) {
+  // Loop through Results Array to select Favorite
+  resultsArray.forEach(item => {
+    if (item.url.includes(itemUrl) && !favorites[itemUrl]) {
+      favorites[itemUrl] = item
+      // Show Save Confirmation for 2 seconds
+      saveConfirmed.hidden = false
+      setTimeout(() => {
+        saveConfirmed.hidden = true
+      }, 2000)
+      // Set Favorites in localStorage
+      localStorage.setItem('nasaFavorites', JSON.stringify(favorites))
+    }
+  })
+}
+
+//On Load
+getNasaPictures()
